c44e68e4a335c97bd3e1f218eebb42271c1c4bd5
Taeyeong Kim <taeyeong.kim@algorix.io>
2025-07-05 11:18:44 -0700

    feat: add sidebar

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -1,8 +1,49 @@
-import { Column } from '@vanta-design/desktop/src/components/layout/column';
-import { fullHeight } from '@vanta-design/desktop/src/styles/utils.css';
-import { cn } from '@vanta-design/desktop/src/utils/common';
-import { wrapper } from './styles.css';
+import { Badge } from '@vanta-design/desktop/src/components/atom/badge';
+import { Button } from '@vanta-design/desktop/src/components/atom/button';
+import { Panel } from '@vanta-design/desktop/src/components/molecule/panel';
+import { BookmarkCheck, Box, FileText, HardDrive, Plus } from 'lucide-react';
 
 export function Sidebar() {
-  return <Column className={cn(wrapper, fullHeight)} shrink={false}></Column>;
+  return (
+    <Panel>
+      <Panel.Header
+        imageSrc='/vanta.png'
+        name='Vanta Design'
+        description='8K members ⋅ Free plan'
+      />
+      <Panel.Section title='Works'>
+        <Panel.Section.Item leadingIcon={Box}>Projects</Panel.Section.Item>
+        <Panel.Section.Item
+          leadingIcon={BookmarkCheck}
+          trailingArea={
+            <Badge.Small semantic='caution'>3 assigned</Badge.Small>
+          }
+        >
+          Tasks
+        </Panel.Section.Item>
+        <Panel.Section.Item
+          leadingIcon={HardDrive}
+          trailingArea={<Badge.Small semantic='positive'>Good</Badge.Small>}
+        >
+          Drive
+        </Panel.Section.Item>
+        <Panel.Section.Item
+          leadingIcon={FileText}
+          trailingArea={<Badge.Small semantic='neutral'>1.5K</Badge.Small>}
+        >
+          Documents
+        </Panel.Section.Item>
+      </Panel.Section>
+      <Panel.Section title='Chats'>
+        <Button.Small variant='bare' leadingIcon={Plus}>
+          Add Chats
+        </Button.Small>
+      </Panel.Section>
+      <Panel.Section title='Directs'>
+        <Button.Small variant='bare' leadingIcon={Plus}>
+          Add Directs
+        </Button.Small>
+      </Panel.Section>
+    </Panel>
+  );
 }
--- src/components/layout/sidebar/styles.css.ts
+++
@@ -1,7 +0,0 @@
-import { style } from '@vanilla-extract/css';
-import { fill } from '@vanta-design/desktop/src/tokens/color.css';
-
-export const wrapper = style({
-  width: 300,
-  backgroundColor: fill.surface.base,
-});
